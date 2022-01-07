8ac907dd3757cd3fd92a9c7af56ca8cc84985ef7
joaquim.neto <joaquim.neto@ati.to.gov.br>
2022-01-07 11:48:56 -0300

    Iniciando repositorio

---
+++ html/index.php
@@ -0,0 +1,19 @@
+<html>
+<?php
+        echo 'Versão Atual do PHP: ' . phpversion();
+
+        $servername = "mysqlASW";
+        $username = "root";
+        $password = "root";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "<br /> Connected successfully";
+?>
+<?='<br>teste'?>
+</html>
